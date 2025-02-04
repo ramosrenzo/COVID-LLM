9af125af1b4fc06c9e3eac3320189082f6f43018
ramosrenzo <somaroznerolnuaj@gmail.com>
2025-02-04 01:51:36 -0800

    removed .yml file, added fecal dataset and tested

--- grover/model.py
+++ grover/model.py
@@ -2,17 +2,23 @@ import pandas as pd
 import numpy as np
 import torch
 import warnings
+import tqdm
 from transformers import AutoTokenizer, AutoModel
 
 warnings.filterwarnings('ignore')
 
 def run_model():
+    # for GPU usage (if available)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f"Using device: {device}")
+
+    # initialize tokenizer and model
     tokenizer = AutoTokenizer.from_pretrained("PoetschLab/GROVER")
-    model = AutoModel.from_pretrained("PoetschLab/GROVER")
+    model = AutoModel.from_pretrained("PoetschLab/GROVER").to(device)
     print("Model successfully loaded.")
 
     dna = "ACGTAGCATCGGATCTATCTATCGACACTTGGTTATCGATCTACGAGCATCTCGTTAGC"
-    inputs = tokenizer(dna, return_tensors = 'pt')["input_ids"]
+    inputs = tokenizer(dna, return_tensors = 'pt')["input_ids"].to(device)
     hidden_states = model(inputs)[0]
     print("Hidden states successfully loaded.")
 
