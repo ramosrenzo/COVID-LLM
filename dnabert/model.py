3577caa86d6e57be1eb860893e73158c88597a00
Carmen Truong <carmentruong222@gmail.com>
2025-02-06 19:13:19 -0800

    update DNABERT model.py

--- dnabert/model.py
+++ dnabert/model.py
@@ -4,6 +4,7 @@ import gc
 import numpy as np
 import pandas as pd
 import warnings
+from tqdm import tqdm
 
 warnings.filterwarnings('ignore')
 logging.set_verbosity_error()
@@ -17,24 +18,32 @@ def run_dnabert():
     clean_gpu()
 
     # initialize tokenizer and model
-    config = BertConfig.from_pretrained('https://raw.githubusercontent.com/jerryji1993/DNABERT/master/src/transformers/dnabert-config/bert-config-6/config.json')
-    tokenizer = AutoTokenizer.from_pretrained("zhihan1996/DNA_bert_6", trust_remote_code=True)
-    model = AutoModel.from_pretrained("zhihan1996/DNA_bert_6", config=config, trust_remote_code=True).to(device)
+    config = BertConfig.from_pretrained('https://raw.githubusercontent.com/jerryji1993/DNABERT/master/src/transformers/dnabert-config/bert-config-6/config.json', trust_remote_code=True)
+    tokenizer = AutoTokenizer.from_pretrained("zhihan1996/DNA_bert_6")
+    model = AutoModel.from_pretrained("zhihan1996/DNA_bert_6", trust_remote_code=True, config=config).to(device)
     print("DNABERT: Model successfully loaded.")
 
-    # get dna embeddings
-    dna = "ACGTAGCATCGGATCTATCTATCGACACTTGGTTATCGATCTACGAGCATCTCGTTAGC"
-    inputs = tokenizer(dna, return_tensors = 'pt')["input_ids"].to(device)
-    hidden_states = model(inputs)[0]
-    print("DNABERT: Hidden states successfully loaded.")
+    # get data
+    data_df = pd.read_csv('data/fecal/fecal_by_sample.csv')
+    data = data_df.drop(columns=['sample']).to_numpy()
+    print("DNABERT: Data successfully loaded.")
 
-    embedding_mean = torch.mean(hidden_states[0], dim=0)
-    print("DNABERT: Embeddings successfully loaded.")
+    # get dna embeddings with mean pooling
+    def calc_embedding_mean(seq):
+        inputs = tokenizer(seq, return_tensors = 'pt')["input_ids"].to(device)
+        hidden_states = model(inputs)[0]
 
-    model_input = tokenizer.encode_plus(dna, add_special_tokens=True, max_length=512)["input_ids"]
-    model_input = torch.tensor(model_input, dtype=torch.long).to(device)
-    model_input = model_input.unsqueeze(0)   # to generate a fake batch with batch size one
+        # embedding with mean pooling
+        embedding_mean = torch.mean(hidden_states[0], dim=0)
+
+        return embedding_mean
     
-    output = model(model_input)
-    print(output[1])
-    print("DNABERT: Completed.")
+    embeddings_list = []
+    for sample in tqdm(data):
+        sample_embeddings = []
+        for seq in tqdm(sample):
+            sample_embeddings.append(calc_embedding_mean(seq).detach().cpu().numpy())
+        embeddings_list.append(sample_embeddings)
+    embeddings = torch.tensor(embeddings_list)
+    print(f"DNABERT: Embeddings successfully loaded.")
+    print(f"DNABERT: Completed.")
