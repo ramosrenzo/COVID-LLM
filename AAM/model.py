b1b428ee88abc243b6fb151e97710a73e86c46a3
mofuwu <swchan@ucsd.edu>
2025-02-21 18:17:22 -0800

    updated model.py

--- AAM/model.py
+++ AAM/model.py
@@ -1,31 +1,22 @@
 from aam.data_handlers.generator_dataset import GeneratorDataset
 from aam.models.transformers import TransformerEncoder
 from aam.models.multihead_attention_pooling import MultiHeadAttentionPooling
+from aam.models.sequence_regressor import SequenceRegressor
+from aam.callbacks import SaveModel
+from keras.callbacks import EarlyStopping
+
 import tensorflow as tf
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split, StratifiedKFold
+import biom
+import os
 import warnings
 
 warnings.filterwarnings('ignore')
 
 K = tf.keras
-
-class ASVWrapper(tf.keras.layers.Layer):
-    def __init__(self, asv_encoder, **kwargs):
-        super(ASVWrapper, self).__init__(**kwargs)
-        self.asv_encoder = asv_encoder
-        self.asv_encoder.trainable = False
-        self.embedding_dim = self.asv_encoder.embedding_dim
-    def call(self, inputs, training=False):
-        embeddings, counts = self.asv_encoder(inputs, return_asv_embeddings=True, training=False)
-        return embeddings, counts
-    def get_config(self):
-        config = super().get_config()
-        config.update({"asv_encoder": tf.keras.saving.serialize_keras_object(self.asv_encoder)})
-    @classmethod
-    def from_config(cls, config):
-        config["asv_encoder"] = tf.keras.saving.deserialize_keras_object(config["asv_encoder"])
-        model = cls(**config)
-        return model
 
 class Classifier(K.Model):
     def __init__(self, feature_extractor, **kwargs):
@@ -34,21 +25,11 @@ class Classifier(K.Model):
         self.feature_extractor = feature_extractor
         self.feature_extractor.trainable = False
 
-        self.encoder = TransformerEncoder(
-            num_layers=4, 
-            num_attention_heads=4, 
-            intermediate_size=512, 
-            dropout_rate=0.1, 
-            normalize_outputs=False,
-            use_residual_connections=True,
-            use_linear_bias=False
-        )  # transformer (or anything else)
-        self.pooling = MultiHeadAttentionPooling(normalize_output=False,  use_linear_bias=False)  # extracts sample embeddings from our ASV embeddings
-        self.dense_ff = K.layers.Dense(1)  # sample_embedings => classifys covid +/-
+        self.model = SequenceRegressor(2048, dropout_rate=0.1, intermediate_size=512, intermediate_activation='gelu', base_model = feature_extractor, out_dim = 1)
 
         self.loss_fn = K.losses.BinaryCrossentropy(from_logits=True)
 
-        self.auc_tracker = K.metrics.Mean()
+        self.auc_tracker = K.metrics.AUC(from_logits=True)
         self.loss_tracker = K.metrics.Mean()
 
     def call(self, inputs, mask=None, training=False):
@@ -56,15 +37,8 @@ class Classifier(K.Model):
         inputs: [B, A, N], B: batch_dim, A: # ASV in sample, N: nuctides,
         string tensor
         """
+        return self.model(inputs, training=training)
 
-
-        # aam case
-        features, counts = self.feature_extractor(inputs)
-        mask =  tf.cast(counts >  0, dtype="float32")
-
-        encodig_output = self.encoder(features, mask=mask, training=training)  # [B, A, N]
-        pooling_output = self.pooling(encodig_output, mask=mask, training=training)  # [B, N]
-        return self.dense_ff(pooling_output)
 
     def train_step(self, data):
         x, y = data
@@ -72,47 +46,53 @@ class Classifier(K.Model):
         # create attention mask
         # example [["ACTG"], [""]]
         with tf.GradientTape() as tape:
-            output = self(x, training=True)
+            sample_embeddings, output = self(x, training=True)
             loss = self.loss_fn(y, output)
 
         gradients = tape.gradient(loss, self.trainable_variables)
         self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
         self.loss_tracker.update_state(loss)
-        self.auc_tracker.update_state(loss)
+        self.auc_tracker.update_state(y, output)
         return {"loss": self.loss_tracker.result(), "auc": self.auc_tracker.result()}
 
     def test_step(self, data):
         x, y = data
-        output = self(x, training=False)
+        sample_embeddings, output = self(x, training=False)
         loss = self.loss_fn(y, output)
 
         self.loss_tracker.update_state(loss)
-        self.auc_tracker.update_state(loss)
+        self.auc_tracker.update_state(y, output)
 
         return {"loss": self.loss_tracker.result(), "auc": self.auc_tracker.result()}
 
     def predict_step(self, data):
         x, y = data
-        return self(x)
+        sample_embeddings, output = self(x, training=False)
+        predictions = tf.reshape(tf.keras.activations.sigmoid(output), shape=[-1])
+        y = tf.reshape(y, shape=[-1])
+        y = tf.cast(y, dtype=tf.float32)
+        pred_label = tf.cast(predictions >= 0.5, dtype=tf.float32)
+        correct = tf.reshape(tf.cast(pred_label == y, dtype=tf.float32), shape=[-1])
+        correct = tf.reduce_mean(correct, axis=0)
+        return pred_label, y, correct
+    def get_config(self):
+        config = super(Classifier, self).get_config()
+        config.update({'feature_extractor': tf.keras.saving.serialize_keras_object(self.feature_extractor), 'build_input_shape': self.get_build_config()})
+        return config
+    def build(self, input_shape):
+        if self.built:
+            return
+        super(Classifier, self).build(input_shape)
+    @classmethod
+    def from_config(cls, config):
+        build_input_shape = config.pop('build_input_shape')
+        input_shape = build_input_shape['input_shape']
+        config['feature_extractor'] = tf.keras.saving.deserialize_keras_object(config['feature_extractor'])
+        model = cls(**config)
+        model.build(input_shape)
+        return model
 
-def run_model():
-    #get data
-    fecal = pd.read_csv('data/fecal.tsv', sep='\t')
-
-    fecal_filtered = fecal[['sample_name', 'covid_positive']]
-    
-    fecal_filtered
-    
-    def check_covid_positive(row):
-        if row =='yes':
-            return 1
-        else:
-            return 0
-
-    fecal_filtered['has_covid'] = fecal_filtered['covid_positive'].apply(check_covid_positive)
-    fecal_filtered.set_index('sample_name', inplace=True)
-
-    def get_dataset(gen):
+def get_dataset(gen):
         enqueuer = tf.keras.utils.OrderedEnqueuer(gen, use_multiprocessing=True)
         enqueuer.start(workers=2, max_queue_size=gen.steps_per_epoch)
         gen.stop = lambda: enqueuer.stop(0.1)
@@ -130,30 +110,115 @@ def run_model():
             ),
         )
         return dataset
+def get_sample_type(file_path):
+            filename = os.path.basename(file_path)
+            # Remove the 'training_metadata_' prefix and the file extension
+            if filename.startswith('training_metadata_'):
+                sample_type = filename[len('training_metadata_'):]
+                sample_type = os.path.splitext(sample_type)[0]
+                return sample_type
+            return "Unknown"
+    
+#function that creates training and valid split and trains each model
+def train_model(train_fp, model=None):
+    print()
+    training_metadata = pd.read_csv(train_fp, sep='\t', index_col=0)
+    X = training_metadata.drop(columns=['study_sample_type', 'has_covid'], axis=1)
+    y = training_metadata[['study_sample_type', 'has_covid']]
+    
+    _, _, y_train, y_valid = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
 
-    gd = GeneratorDataset(
-        table='/home/swchan/DSC170/data/input/merged_biom_table.biom',
-        metadata='/home/swchan/DSC170/data/input/training_metadata.tsv',
-        metadata_column='has_covid',
-        shuffle=False,
-        is_categorical=False,
-        shift=0,
-        rarefy_depth = 5000,
-        scale=1,
-        batch_size=4
-    )
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
-    dataset = get_dataset(gd)
+    curr_best_val_loss = np.inf
+    curr_best_model = None
+    for i, (train_index, valid_index) in enumerate(skf.split(y, y['has_covid'])):
+        y_train = y.iloc[train_index]
+        y_valid = y.iloc[valid_index]
+        
+        gd_train = GeneratorDataset(
+            table='data/input/merged_biom_table.biom',
+            metadata=y_train,
+            metadata_column='has_covid',
+            shuffle=False,
+            is_categorical=False,
+            shift=0,
+            rarefy_depth = 4000,
+            scale=1,
+            batch_size = 4,
+            gen_new_tables = True #only in training dataset
+        )
+    
+        gd_valid = GeneratorDataset(
+            table='data/input/merged_biom_table.biom',
+            metadata=y_valid,
+            metadata_column='has_covid',
+            shuffle=False,
+            is_categorical=False,
+            shift=0,
+            rarefy_depth = 4000,
+            scale=1,
+            batch_size = 4,
+        )
+    
+        dataset_train = get_dataset(gd_train)
+        dataset_valid = get_dataset(gd_valid)
+  
+        def get_sample_type(file_path):
+            filename = os.path.basename(file_path)
+            # Remove the 'training_metadata_' prefix and the file extension
+            if filename.startswith('training_metadata_'):
+                sample_type = filename[len('training_metadata_'):]
+                sample_type = os.path.splitext(sample_type)[0]
+                return sample_type
+            return "Unknown"
 
-    base_model = tf.keras.models.load_model('/home/swchan/DSC170/model.keras', compile=False)
-    base_model = ASVWrapper(base_model)
+        if model is None:
+            base_model = tf.keras.models.load_model('model.keras', compile=False)
+            model = Classifier(base_model)
+        
+        token_shape = tf.TensorShape([None, 150])
+        batch_indicies = tf.TensorShape([None, 2])
+        indicies_shape = tf.TensorShape([None])
+        count_shape = tf.TensorShape([None, 1])
+        model.build([token_shape, batch_indicies, indicies_shape, count_shape])
+    
+        optimizer = tf.keras.optimizers.Adam(
+            learning_rate=tf.keras.optimizers.schedules.CosineDecay(
+            initial_learning_rate = 0.0,
+            warmup_target = 0.0003, # maybe change
+            warmup_steps=0,
+            decay_steps=100000
+        )
+)
+    
+        model.compile(optimizer=optimizer, run_eagerly=False)
+        #switch loss to val loss 
+        #pass early stopping for callbacks
+        history = model.fit(dataset_train, 
+                  validation_data = dataset_valid, 
+                  validation_steps=gd_valid.steps_per_epoch, 
+                  epochs=10000, 
+                  steps_per_epoch=gd_train.steps_per_epoch, 
+                  callbacks=[
+                             EarlyStopping(patience=250, start_from_epoch=0, restore_best_weights=True)
+                            ])
 
-    model = Classifier(base_model)
+        validation_loss = history.history['val_loss']
+        epochs = np.array(range(len(validation_loss)))
+        plt.plot(epochs, validation_loss)
+        plt.title(f'Validation Loss Per Epoch {validation_loss[-1]}')
+        plt.savefig(f'trained_models/{get_sample_type(train_fp)}/{get_sample_type(train_fp)}_{i}_model_loss.png')
+        plt.close()
+        if history.history['val_loss'][-1] < curr_best_val_loss:
+            curr_best_model = model
+        model.save(f'trained_models/{get_sample_type(train_fp)}/{get_sample_type(train_fp)}_{i}_model.keras', save_format='keras')
+    curr_best_model.save(f'trained_models/{get_sample_type(train_fp)}/{get_sample_type(train_fp)}_best_model.keras', save_format='keras')
 
-    optimizer = K.optimizers.Adam(
-        learning_rate=1e-4
-    )
+def run_model():
+    #get data
 
-    model.compile(optimizer=optimizer, run_eagerly=False)
-
-    model.fit(dataset, epochs=10, steps_per_epoch=gd.steps_per_epoch)
+    train_model('data/input/training_metadata_nares.tsv')
+    train_model('data/input/training_metadata_stool.tsv')
+    train_model('data/input/training_metadata_inside_floor.tsv')
+    train_model('data/input/training_metadata_forehead.tsv')
